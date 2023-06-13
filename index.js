60592df99bc53390ae0e8d2495c50945ee22e08b
simonstreuli <112710583+simonstreuli@users.noreply.github.com>
2023-06-13 13:47:24 +0200

    MongoDB connection woriking

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const helmet = require("helmet");
+const morgan = require("morgan");
+
+dotenv.config();
+
+const PORT = process.env.PORT || 6001;
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+  })
+  .catch((error) => console.log(`${error} did not connect`));

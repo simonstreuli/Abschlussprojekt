a70c3b1995ceda8be5ad5a36f3df23a3acd3ca3f
simonstreuli <112710583+simonstreuli@users.noreply.github.com>
2023-06-13 14:31:18 +0200

    Created user model

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
+const User = require("./models/User");
 
-router.get("/", (req, res) => {
+//Register
+router.get("/register", (req, res) => {
   res.send("auth route");
 });
 

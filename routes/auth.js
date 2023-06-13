18c6510c933921fceabad9c15192718d25976daa
simonstreuli <112710583+simonstreuli@users.noreply.github.com>
2023-06-13 14:41:38 +0200

    reigister working

--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,19 @@
 const router = require("express").Router();
-const User = require("./models/User");
+const User = require("../models/User");
 
 //Register
-router.get("/register", (req, res) => {
-  res.send("auth route");
+router.post("/register", async (req, res) => {
+  const newUser = new User({
+    username: req.body.username,
+    email: req.body.email,
+    password: req.body.password,
+  });
+  try {
+    const user = await newUser.save();
+    res.status(200).json(user);
+  } catch (err) {
+    console.error(err);
+  }
 });
 
 module.exports = router;

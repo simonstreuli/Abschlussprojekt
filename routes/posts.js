a1190ac598c4a4bad8514dd4d7a989ed9b1563ee
simonstreuli <112710583+simonstreuli@users.noreply.github.com>
2023-06-14 09:08:51 +0200

    CRUD Posts

--- routes/posts.js
+++ routes/posts.js
@@ -35,14 +35,53 @@ router.delete("/:id", async (req, res) => {
       await post.deleteOne();
       res.status(200).json("the post has been deleted");
     } else {
-      res.status(403).json("you can update only your post");
+      res.status(403).json("you can delete only your post");
     }
   } catch (err) {
     res.status(500).json(err);
   }
 });
+
 //like post
+router.put("/:id/like", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (!post.likes.includes(req.body.userId)) {
+      await post.updateOne({ $push: { likes: req.body.userId } });
+      res.status(200).json("Post liked");
+    } else {
+      await post.updateOne({ $pull: { likes: req.body.userId } });
+      res.status(200).json("Post diskliked");
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 //get post
+router.get("/:id", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    res.status(200).json(post);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 //get timeline posts
+router.get("/timeline/all", async (req, res) => {
+  try {
+    const currentUser = await User.findById(req.body.userId);
+    const userPosts = await Post.find({ userId: currentUser._id });
+    const friendPosts = await Promise.all(
+      currentUser.followings.map((friendId) => {
+        return Post.find({ userId: friendId });
+      })
+    );
+    res.json(userPosts.concat(...friendPosts));
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 module.exports = router;

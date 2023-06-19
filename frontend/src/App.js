7a3dfe91209956b79b9fa5fa9b4140976fb99695
simonstreuli <112710583+simonstreuli@users.noreply.github.com>
2023-06-19 10:20:45 +0200

    Register done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,9 @@
 import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
+
 function App() {
-  return <Login />;
+  return <Home />;
 }
 
 export default App;
--- frontend/src/pages/register/Register.jsx
+++ frontend/src/pages/register/Register.jsx
@@ -1,7 +1,29 @@
-import "./register.css"
+import "./register.css";
 
 export default function Register() {
   return (
-    <div>Register</div>
-  )
+    <div className="login">
+      <div className="loginWrapper">
+        <div className="loginLeft">
+          <h3 className="loginLogo">LinkUp</h3>
+          <span className="loginDesc">
+          Discover, Connect, Share!
+
+          </span>
+        </div>
+        <div className="loginRight">
+          <div className="loginBox">
+            <input placeholder="Username" className="loginInput" />
+            <input placeholder="Email" className="loginInput" />
+            <input placeholder="Password" className="loginInput" />
+            <input placeholder="Password Again" className="loginInput" />
+            <button className="loginButton">Sign Up</button>
+            <button className="loginRegisterButton">
+              Log into Account
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }

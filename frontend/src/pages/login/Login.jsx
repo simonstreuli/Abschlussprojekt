809465a25c7341af2245137238ae5aa65530c1c8
simonstreuli <112710583+simonstreuli@users.noreply.github.com>
2023-06-19 09:33:37 +0200

    login start

---
+++ frontend/src/pages/login/Login.jsx
@@ -0,0 +1,17 @@
+import "./login.css"
+
+export default function Login() {
+  return (
+    <div className="login">
+        <div className="wrapper">
+            <div className="loginLeft">
+                <img src="../../assets/logo.png" alt="" className="logo" />
+                <span className="loginDesc">.....asdas</span>
+            </div>
+            <div className="loginRight">
+                <div className="loginBox"></div>
+            </div>
+        </div>
+    </div>
+  )
+}

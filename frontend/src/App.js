c2214b415290e8c6019260e5ba069543fa0a51a0
simonstreuli <112710583+simonstreuli@users.noreply.github.com>
2023-06-19 11:44:12 +0200

    Routes working

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,22 @@
 import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
 import Register from "./pages/register/Register";
+import {
+  BrowserRouter,
+  Routes, // instead of "Switch"
+  Route,
+} from "react-router-dom";
 
 function App() {
-  return <Home />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;

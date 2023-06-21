6a5c394a5a89151fdb1408adbd09cc66e16f9365
simonstreuli <112710583+simonstreuli@users.noreply.github.com>
2023-06-21 08:22:09 +0200

    navbar update

--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -2,8 +2,12 @@ import "./navbar.css";
 import logo from "../../assets/logo.png";
 import { Person, Chat, Notifications } from "@material-ui/icons";
 import { Link } from "react-router-dom";
+import { AuthContext } from "../../context/AuthContext";
+import { useContext } from "react";
 
 export default function Navbar() {
+  const publicFolder = process.env.REACT_APP_PUBLIC_FOLDER;
+  const { user } = useContext(AuthContext);
   return (
     <div className="navbar">
       <div className="navbarLeft">
@@ -15,8 +19,9 @@ export default function Navbar() {
       </div>
       <div className="navbarCenter">
         <div className="navbarlinks">
-          <span className="navbar-link">Home</span>
-          <span className="navbar-link">Timeline</span>
+          <Link to="/" className="navbar-link">
+            <span className="navbar-link">Home</span>
+          </Link>
         </div>
       </div>
       <div className="navbarRight">
@@ -34,11 +39,17 @@ export default function Navbar() {
             <span className="navbarbadge">1</span>
           </div>
         </div>
-        <img
-          src="/assets/person/2.jpeg"
-          alt=""
-          className="navbar-profile-img"
-        />
+        <Link to={`/profile/${user.username}`}>
+          <img
+            src={
+              user.profilePicture
+                ? publicFolder + user.profilePicture
+                : publicFolder + "person/noAvatar.png"
+            }
+            alt=""
+            className="navbar-profile-img"
+          />
+        </Link>
       </div>
     </div>
   );
